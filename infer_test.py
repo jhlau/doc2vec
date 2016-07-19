95a9527b3e951d05282a291662bafee1fce8db86
root <root@kannazuki.sl.cloud9.ibm.com>
2016-07-19 11:40:59 +1000

    adding code to train/test models

---
+++ infer_test.py
@@ -0,0 +1,23 @@
+#python example to infer document vectors from trained doc2vec model
+import gensim.models as g
+import codecs
+
+#parameters
+model="toy_data/model.bin"
+test_docs="toy_data/test_docs.txt"
+output_file="toy_data/test_vectors.txt"
+
+#inference hyper-parameters
+start_alpha=0.01
+infer_epoch=1000
+
+#load model
+m = g.Doc2Vec.load(model)
+test_docs = [ x.strip() for x in codecs.open(test_docs, "r", "utf-8").readlines() ]
+
+#infer test vectors
+output = open(output_file, "w")
+for d in test_docs:
+    output.write( " ".join([str(x) for x in m.infer_vector(d, alpha=start_alpha, steps=infer_epoch)]) + "\n" )
+output.flush()
+output.close()
---
+++ train_model.py
@@ -0,0 +1,33 @@
+#python example to train doc2vec model (with or without pre-trained word embeddings)
+
+import gensim.models as g
+import logging
+
+#doc2vec parameters
+vector_size = 300
+window_size = 15
+min_count = 1
+sampling_threshold = 1e-5
+negative_size = 5
+train_epoch = 100
+dm = 0 #0 = dbow; 1 = dmpv
+worker_count = 1 #number of parallel processes
+
+#pretrained word embeddings
+pretrained_emb = "toy_data/pretrained_word_embeddings.txt" #None if use without pretrained embeddings
+
+#input corpus
+train_corpus = "toy_data/train_docs.txt"
+
+#output model
+saved_path = "toy_data/model.bin"
+
+#enable logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+#train doc2vec model
+docs = g.doc2vec.TaggedLineDocument(train_corpus)
+model = g.Doc2Vec(docs, size=vector_size, window=window_size, min_count=min_count, sample=sampling_threshold, workers=worker_count, hs=0, dm=dm, negative=negative_size, dbow_words=1, dm_concat=1, pretrained_emb=pretrained_emb)
+
+#save model
+model.save(saved_path)
