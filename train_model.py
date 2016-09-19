48fecc34e4b3c55806107153b698337a77033805
jhlau <depthchargex@gmail.com>
2016-09-19 10:19:38 +1000

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -27,7 +27,7 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 #train doc2vec model
 docs = g.doc2vec.TaggedLineDocument(train_corpus)
-model = g.Doc2Vec(docs, size=vector_size, window=window_size, min_count=min_count, sample=sampling_threshold, workers=worker_count, hs=0, dm=dm, negative=negative_size, dbow_words=1, dm_concat=1, pretrained_emb=pretrained_emb)
+model = g.Doc2Vec(docs, size=vector_size, window=window_size, min_count=min_count, sample=sampling_threshold, workers=worker_count, hs=0, dm=dm, negative=negative_size, dbow_words=1, dm_concat=1, pretrained_emb=pretrained_emb, iter=train_epoch)
 
 #save model
 model.save(saved_path)
